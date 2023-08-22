fe5551b9250e81017bd1c74177703276e5a8e7e4
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2023-08-22 16:32:17 +0900

    added explanation comments to the contains duplicate solution

--- Arrays & Hashing/Contains Duplicate/index.js
+++ Arrays & Hashing/Contains Duplicate/index.js
@@ -1,8 +1,12 @@
 const containsDuplicate = (nums) => {
+  // create array for unique elements storage
   let arr = []
+  // loop over given array
   nums.forEach((el) => {
+    // push unique elements to new array
     if (!arr.includes(el)) arr.push(el)
   })
+  // check if the new unique element's array length is equal to given array length
   return arr.length !== nums.length
 }
 
